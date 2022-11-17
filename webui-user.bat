3d946ecb446697ed44851d85f823ab5b042de56f
mihau2 <mrapczynski@web.de>
2022-11-17 18:06:57 +0100

    added xformers

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--vae-path "E:\AI\automatic1111\stable-diffusion-webui\models\vae-ft-mse-840000-ema-pruned.vae.pt" --disable-safe-unpickle --deepdanbooru
+set COMMANDLINE_ARGS=--vae-path "E:\AI\automatic1111\stable-diffusion-webui\models\vae-ft-mse-840000-ema-pruned.vae.pt" --disable-safe-unpickle --deepdanbooru --xformers
 
 call webui.bat

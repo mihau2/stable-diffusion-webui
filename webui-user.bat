ce40a9b8913f96fe7d4e7427f4345f412ca8d1dd
mihau2 <mrapczynski@web.de>
2022-11-26 19:55:58 +0100

    asd

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--vae-path "E:\AI\automatic1111\stable-diffusion-webui\models\vae-ft-mse-840000-ema-pruned.vae.pt" --disable-safe-unpickle --deepdanbooru --xformers
+set COMMANDLINE_ARGS= --deepdanbooru --xformers
 
+git pull
 call webui.bat
